8cf353263d8fa623cf4973f3f090f1eb1febaa15
Pavithran <pavithranthilakanathan@gmai.com>
2023-01-06 17:26:54 +1100

    conf.py version update

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,7 @@ sys.path.insert(0, os.path.abspath(".."))
 project = "Cricguru"
 copyright = "2023, Pavithran Thilakanathan"
 author = "Pavithran Thilakanathan"
-release = "1.1.0"
+release = "1.3.0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
